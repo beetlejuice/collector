06e3c86c25d86794e998d188c2f5edf3a6cd9564
Andrew Rymar <anr@qapint.com>
2015-04-06 15:19:41 +0300

    Modify prepare_body to work with multiple texts

--- lib/speller.rb
+++ lib/speller.rb
@@ -6,25 +6,26 @@ module Speller
   API_URL = 'http://speller.yandex.net'
   OPTIONS = '0'
 
-  def spellcheck text, mode = 'html'
+  def spellcheck texts, mode = 'html'
     check_settings = {options: OPTIONS, format: mode}
 
     uri = URI.parse(API_URL)
     http = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Post.new('/services/spellservice.json/checkText')
-    req.body = prepare_body text, check_settings
+    req = Net::HTTP::Post.new('/services/spellservice.json/checkTexts')
+    req.body = prepare_body texts, check_settings
     res = http.request(req)
 
     # raise "Bad server response! - #{res.code}" unless res == Net::HTTPSuccess
 
     parsed_response = JSON.parse(res.body)
-    {has_errors: !parsed_response.empty?, response: parsed_response}
+    # {has_errors: !parsed_response.empty?, response: parsed_response}
   end
 
   private
 
-  def prepare_body body, settings
+  def prepare_body texts, settings
     # Optionally format encoding here
-    'text=' + body + '&options=' + settings[:options] + '&format=' + settings[:format]
+    text_body = 'text=' + texts.join('&text=')
+    text_body + '&options=' + settings[:options] + '&format=' + settings[:format]
   end
 end
